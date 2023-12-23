7c8be0d697807e64803884fb26ece297f14f54c9
Juan Manuel <juanph.dv@gmail.com>
2023-12-23 00:54:35 -0500

    sfs

--- slash/ping.js
+++ slash/ping.js
@@ -1,11 +1,20 @@
-const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, TextInputBuilder, ModalBuilder, TextInputStyle, InteractionType, SlashCommandBuilder, SelectMenuBuilder} = require("discord.js");
+const {
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  TextInputBuilder,
+  ModalBuilder,
+  TextInputStyle,
+  InteractionType,
+  SlashCommandBuilder,
+  SelectMenuBuilder,
+} = require("discord.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("ping")
-        .setDescription("Replies with pong!"),
+  data: new SlashCommandBuilder().setName("hello").setDescription("hola perro"),
 
-    run: async ({ client, interaction }) => {  
-        return interaction.reply("Pong!");
-    },
+  run: async ({client, interaction}) => {
+    return interaction.reply("Pong!");
+  },
 };
